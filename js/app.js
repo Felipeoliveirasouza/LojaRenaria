47ad59c2c925cc8c345d2cf32dc7ad62fd10df3d
Felipe Oliveira Souza <153692420+Felipeoliveirasouza@users.noreply.github.com>
2025-06-02 14:57:13 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -320,9 +320,9 @@ produtos.metodos = {
 
         VALOR_CARRINHO = 0;
 
-        $("lblSubtotal").text("R$ 0,00");
+        $("#lblSubtotal").text("R$ 0,00");
         // $("lblValorEntrega").text("Uber Flash");
-        $("lblValorTotal").text("R$ 0,00");
+        $("#lblValorTotal").text("R$ 0,00");
 
         $.each(MEU_CARRINHO, (i, e) => {
 
@@ -330,8 +330,8 @@ produtos.metodos = {
 
             if ((i + 1) == MEU_CARRINHO.length) {
                 $("#lblSubtotal").text("R$ " + VALOR_CARRINHO.toFixed(2).replace('.', ','));
-                $("#lblValorEntrega").text("+ R$ " + VALOR_ENTREGA.toFixed(2).replace('.', ','));
-                $("#lblValorTotal").text("R$ " + (VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.', ','));
+                // $("#lblValorEntrega").text("+ R$ " + VALOR_ENTREGA.toFixed(2).replace('.', ','));
+                $("#lblValorTotal").text("R$ " + (VALOR_CARRINHO).toFixed(2).replace('.', ','));
             }
 
         });
