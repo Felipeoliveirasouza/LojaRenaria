c8b7b793ef328dd4a4fc752fed777e8a59a07165
Felipe Oliveira Souza <felipeprogramadordev@gmail.com>
2025-06-03 21:26:57 -0300

    atualização

--- js/dados.js
+++ js/dados.js
@@ -3,73 +3,11 @@ var MENU = {
         {
             "id": "cosmeticos-1",
             "img": "./img/cardapio/cosmeticos/cosmeticos (1).png",
-            "name": "cosméticos (1)",
-            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
-            "price": 99.90
+            "name": "esgotado",
+            "dsc": "",
+            "price": 0.00
         },
-        {
-            "id": "cosmeticos-2",
-            "img": "./img/cardapio/cosmeticos/cosmeticos (2).png",
-            "name": "cosméticos (2)",
-            "dsc": "Carolina BBQ Oink Sampler",
-            "price": 99.90
-        },
-        {
-            "id": "cosmeticos-3",
-            "img": "./img/cardapio/cosmeticos/cosmeticos (3).png",
-            "name": "cosméticos (3)",
-            "dsc": "Texas Monthly's #1 BBQ Brisket",
-            "price": 99.90
-        },
-        {
-            "id": "cosmeticos-4",
-            "img": "./img/cardapio/cosmeticos/cosmeticos (4).png",
-            "name": "cosméticos (4)",
-            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
-            "price": 99.90
-        },
-        {
-            "id": "cosmeticos-5",
-            "img": "./img/cardapio/cosmeticos/cosmeticos (5).png",
-            "name": "cosméticos (5)",
-            "dsc": "Whole Texas Smoked Brisket",
-            "price": 99.90
-        },
-        {
-            "id": "cosmeticos-6",
-            "img": "./img/cardapio/cosmeticos/cosmeticos (6).png",
-            "name": "cosméticos (6)",
-            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
-            "price": 99.90
-        },
-        {
-            "id": "cosmeticos-7",
-            "img": "./img/cardapio/cosmeticos/cosmeticos (7).png",
-            "name": "cosméticos (7)",
-            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
-            "price": 99.90
-        },
-        {
-            "id": "cosmeticos-8",
-            "img": "./img/cardapio/cosmeticos/cosmeticos (8).png",
-            "name": "cosméticos (8)",
-            "dsc": "BBQ Meat Lovers Special for 10",
-            "price": 99.90
-        },
-        {
-            "id": "cosmeticos-9",
-            "img": "./img/cardapio/cosmeticos/cosmeticos (9).png",
-            "name": "cosméticos (9)",
-            "dsc": "The Big Ugly BBQ Dinner for 6",
-            "price": 99.90
-        },
-        {
-            "id": "cosmeticos-10",
-            "img": "./img/cardapio/cosmeticos/cosmeticos (10).png",
-            "name": "cosméticos (10)",
-            "dsc": "Mighty Quinn's BBQ Sampler Pack",
-            "price": 99.90
-        }
+        
         
     ],
     "lingeries": [
@@ -177,291 +115,43 @@ var MENU = {
         {
             "id": "proteses-1",
             "img": "./img/cardapio/proteses/proteses (1).png",
-            "name": "proteses (1)",
-            "dsc": "German Chocolate Killer Brownie®",
-            "price": 50
+            "name": "esgotado",
+            "dsc": "",
+            "price": 0.00
         },
-        {
-            "id": "proteses-2",
-            "img": "./img/cardapio/proteses/proteses (2).png",
-            "name": "proteses (2)",
-            "dsc": "descrição",
-            "price": 50
-        },
-        {
-            "id": "proteses-3",
-            "img": "./img/cardapio/proteses/proteses (3).png",
-            "name": "proteses (3)",
-            "dsc": "Original Cannoli Pie",
-            "price": 50
-        },
-        {
-            "id": "proteses-4",
-            "img": "./img/cardapio/proteses/proteses (4).png",
-            "name": "proteses (4)",
-            "dsc": "Sea-Salted Caramel Swirl Cheesecake",
-            "price": 50
-        },
-        {
-            "id": "proteses-5",
-            "img": "./img/cardapio/proteses/proteses (5).png",
-            "name": "proteses (5)",
-            "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
-            "price": 50
-        },
-        {
-            "id": "proteses-6",
-            "img": "./img/cardapio/proteses/proteses (6).png",
-            "name": "proteses (6)",
-            "dsc": "Best Seller Cupcake Dozen",
-            "price": 50
-        },
-        {
-            "id": "proteses-7",
-            "img": "./img/cardapio/proteses/proteses (7).png",
-            "name": "proteses (7)",
-            "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
-            "price": 50
-        },
-        {
-            "id": "proteses-8",
-            "img": "./img/cardapio/proteses/proteses (8).png",
-            "name": "proteses (8)",
-            "dsc": "Jewish Classics Dessert Pack",
-            "price": 50
-        },
-        {
-            "id": "proteses-9",
-            "img": "./img/cardapio/proteses/proteses (9).png",
-            "name": "proteses (9)",
-            "dsc": "Dessert Bar Care Package",
-            "price": 50
-        },
-        {
-            "id": "proteses-10",
-            "img": "./img/cardapio/proteses/proteses (10).png",
-            "name": "proteses (10)",
-            "dsc": "Donut Cookies - 12 Pack",
-            "price": 50
-        }
+        
       
     ],
     "vibradores": [
         {
             "id": "vibradores-1",
             "img": "./img/cardapio/vibradores/vibradores (1).png",
-            "name": "vibradores (1)",
-            "dsc": "Hong Kong Boba Tea Kit for 6",
-            "price": 60
+            "name": "esgotado",
+            "dsc": "",
+            "price": 0.00
         },
-        {
-            "id": "vibradores-2",
-            "img": "./img/cardapio/vibradores/vibradores (2).png",
-            "name": "vibradores (2)",
-            "dsc": "Guy's Caliente Margaritas for 12",
-            "price": 60
-        },
-        {
-            "id": "vibradores-3",
-            "img": "./img/cardapio/vibradores/vibradores (3).png",
-            "name": "vibradores (3)",
-            "dsc": "Woodford Reserve Mint Julep Syrup",
-            "price": 60
-        },
-        {
-            "id": "vibradores-4",
-            "img": "./img/cardapio/vibradores/vibradores (4).png",
-            "name": "vibradores (4)",
-            "dsc": "New Orleans Hurricane Mix",
-            "price": 60
-        },
-        {
-            "id": "vibradores-5",
-            "img": "./img/cardapio/vibradores/vibradores (5).png",
-            "name": "vibradores (5)",
-            "dsc": "Margarita Mix",
-            "price": 60
-        },
-        {
-            "id": "vibradores-6",
-            "img": "./img/cardapio/vibradores/vibradores (6).png",
-            "name": "vibradores (6)",
-            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
-            "price": 60
-        },
-        {
-            "id": "vibradores-7",
-            "img": "./img/cardapio/vibradores/vibradores (7).png",
-            "name": "vibradores (7)",
-            "dsc": "Unicorn Parade Milkshake Kit for 8",
-            "price": 60
-        },
-        {
-            "id": "vibradores-8",
-            "img": "./img/cardapio/vibradores/vibradores (8).png",
-            "name": "vibradores (8)",
-            "dsc": "Chickpea Chiller Kit for 6",
-            "price": 60
-        },
-        {
-            "id": "vibradores-9",
-            "img": "./img/cardapio/vibradores/vibradores (9).png",
-            "name": "vibradores (9)",
-            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
-            "price": 60
-        },
-        {
-            "id": "vibradores-10",
-            "img": "./img/cardapio/vibradores/vibradores (10).png",
-            "name": "vibradores (10)",
-            "dsc": "Kentucky Derby Mint Julep Gift Set",
-            "price": 60
-        }
+        
     ],
     "sado": [
         {
             "id": "sado-1",
             "img": "./img/cardapio/sado/sado (1).png",
-            "name": "sado (1)",
-            "dsc": "Hong Kong Boba Tea Kit for 6",
-            "price": 60
+            "name": "esgotado",
+            "dsc": "",
+            "price": 0.00
         },
-        {
-            "id": "sado-2",
-            "img": "./img/cardapio/sado/sado (2).png",
-            "name": "sado (2)",
-            "dsc": "Guy's Caliente Margaritas for 12",
-            "price": 60
-        },
-        {
-            "id": "sado-3",
-            "img": "./img/cardapio/sado/sado (3).png",
-            "name": "sado (3)",
-            "dsc": "Woodford Reserve Mint Julep Syrup",
-            "price": 60
-        },
-        {
-            "id": "sado-4",
-            "img": "./img/cardapio/sado/sado (4).png",
-            "name": "sado (4)",
-            "dsc": "New Orleans Hurricane Mix",
-            "price": 60
-        },
-        {
-            "id": "sado-5",
-            "img": "./img/cardapio/sado/sado (5).png",
-            "name": "sado (5)",
-            "dsc": "Margarita Mix",
-            "price": 60
-        },
-        {
-            "id": "sado-6",
-            "img": "./img/cardapio/sado/sado (6).png",
-            "name": "sado (6)",
-            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
-            "price": 60
-        },
-        {
-            "id": "sado-7",
-            "img": "./img/cardapio/sado/sado (7).png",
-            "name": "sado (7)",
-            "dsc": "Unicorn Parade Milkshake Kit for 8",
-            "price": 60
-        },
-        {
-            "id": "sado-8",
-            "img": "./img/cardapio/sado/sado (8).png",
-            "name": "sado (8)",
-            "dsc": "Chickpea Chiller Kit for 6",
-            "price": 60
-        },
-        {
-            "id": "sado-9",
-            "img": "./img/cardapio/sado/sado (9).png",
-            "name": "sado (9)",
-            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
-            "price": 60
-        },
-        {
-            "id": "sado-10",
-            "img": "./img/cardapio/sado/sado (10).png",
-            "name": "sado (10)",
-            "dsc": "Kentucky Derby Mint Julep Gift Set",
-            "price": 60
-        }
+        
        
     ],
     "acessorios": [
         {
             "id": "acessorios-1",
             "img": "./img/cardapio/acessorios/acessorios (1).png",
-            "name": "acessorios (1)",
-            "dsc": "Hong Kong Boba Tea Kit for 6",
-            "price": 60
+            "name": "esgotado",
+            "dsc": "",
+            "price": 0.00
         },
-        {
-            "id": "acessorios-2",
-            "img": "./img/cardapio/acessorios/acessorios (2).png",
-            "name": "acessorios (2)",
-            "dsc": "Guy's Caliente Margaritas for 12",
-            "price": 60
-        },
-        {
-            "id": "acessorios-3",
-            "img": "./img/cardapio/acessorios/acessorios (3).png",
-            "name": "acessorios (3)",
-            "dsc": "Woodford Reserve Mint Julep Syrup",
-            "price": 60
-        },
-        {
-            "id": "acessorios-4",
-            "img": "./img/cardapio/acessorios/acessorios (4).png",
-            "name": "acessorios (4)",
-            "dsc": "New Orleans Hurricane Mix",
-            "price": 60
-        },
-        {
-            "id": "acessorios-5",
-            "img": "./img/cardapio/acessorios/acessorios (5).png",
-            "name": "acessorios (5)",
-            "dsc": "Margarita Mix",
-            "price": 60
-        },
-        {
-            "id": "acessorios-6",
-            "img": "./img/cardapio/acessorios/acessorios (6).png",
-            "name": "acessorios (6)",
-            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
-            "price": 60
-        },
-        {
-            "id": "acessorios-7",
-            "img": "./img/cardapio/acessorios/acessorios (7).png",
-            "name": "acessorios (7)",
-            "dsc": "Unicorn Parade Milkshake Kit for 8",
-            "price": 60
-        },
-        {
-            "id": "acessorios-8",
-            "img": "./img/cardapio/acessorios/acessorios (8).png",
-            "name": "acessorios (8)",
-            "dsc": "Chickpea Chiller Kit for 6",
-            "price": 60
-        },
-        {
-            "id": "acessorios-9",
-            "img": "./img/cardapio/acessorios/acessorios (9).png",
-            "name": "acessorios (9)",
-            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
-            "price": 60
-        },
-        {
-            "id": "acessorios-10",
-            "img": "./img/cardapio/acessorios/acessorios (10).png",
-            "name": "acessorios (10)",
-            "dsc": "Kentucky Derby Mint Julep Gift Set",
-            "price": 60
-        }
+       
        
     ]
 }
